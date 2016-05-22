39dddb46c24e8946f96f0c9c73d894efffc03453
shisaq <shisaq@gmail.com>
2016-05-22 10:33:04 -0400

    build 4 objects

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,122 @@
-/*
-This is empty on purpose! Your code to build the resume will go here.
- */
+/**
+* Define 4 objects: bio, work, projects, education
+* @shisaq
+*/
+var bio = {
+  "name": "Shisaq Lee",
+  "role": "Front-end Developer",
+  "contacts": {
+    "mobile": "412-500-2278",
+    "email": "shisaq@gmail.com",
+    "twitter": "@shisaq",
+    "github": "shisaq",
+    "blog": "shisaq.com",
+    "location": "Pittsburgh, PA, USA"
+  },
+  "picture_URL": "images/carton.jpg",
+  "welcomeMessage": "This picture is just for fun! XD",
+  "skills": [
+    "Ukulele",
+    "HTML",
+    "WordPress",
+    "Premiere",
+    "Photoshop"
+  ]
+};
+
+var work = {
+  "jobs": [
+    {
+      "employer": "Tsinghua University",
+      "title": "Lab Manager",
+      "location": "Beijing, China",
+      "dates": "Sep 1st, 2015 -- Feb 28th, 2016",
+      "description": "I was in charge of Yigong Shi's laboratory"
+    },
+    {
+      "employer": "Peking University",
+      "title": "Program Manager",
+      "location": "Beijing, China",
+      "dates": "Sep 1st, 2014 -- Aug 31th, 2015",
+      "description": "I was in charge of the website of Social Media Center"
+    },
+    {
+      "employer": "CTVIT",
+      "title": "IT Consultant",
+      "location": "Beijing, China",
+      "dates": "Feb 1st, 2012 -- Feb 28th, 2013",
+      "description": "I was in charge of News Prodect System"
+    }
+  ]
+};
+
+var projects = {
+  "projects": [
+    {
+      "title": "My First Website",
+      "dates": "May 1st, 2016 -- May 18th, 2016",
+      "description": "I learned the technique in Udacity",
+      "images": [
+        "images/001.jpg",
+        "images/002.jpg",
+        "images/003.jpg",
+        "images/004.jpg"
+      ]
+    },
+    {
+      "title": "Lab Website",
+      "dates": "Jur 1st, 2016 -- May 17th, 2016",
+      "description": "This is the first website I established",
+      "images": [
+        "images/005.jpg",
+        "images/006.jpg",
+        "images/007.jpg",
+        "images/008.jpg"
+      ]
+    }
+  ]
+};
+
+var education = {
+  "schools": [
+    {
+      "name": "Nanyang Institute of Technology",
+      "location": "Beijing, China",
+      "degree": "Bechelor",
+      "majors": [
+        "Software Engineering",
+        "Chinese",
+        "Math"
+      ],
+      "dates": 2008,
+      "url": "http://soft-bysj.nyist.net/"
+    },
+    {
+      "name": "Nanzhao High School",
+      "location": "Nanyang, China",
+      "degree": "N.O.",
+      "majors": [
+        "English",
+        "Chinese",
+        "Hostory",
+        "Math"
+      ],
+      "dates": 2005,
+      "url": "http://www.nzxdzx.com/"
+    }
+  ],
+  "onlineCourses": [
+    {
+      "title": "Front-end Nanodegree",
+      "school": "Udacity",
+      "dates": 2016,
+      "url": "https://www.udacity.com/course/front-end-web-developer-nanodegree--nd001"
+    },
+    {
+      "title": "Intro to HTML and CSS",
+      "school": "Udacity",
+      "dates": 2015,
+      "url": "https://www.udacity.com/course/intro-to-html-and-css--ud304"
+    }
+  ]
+};
